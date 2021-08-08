94f2c13bfa673772da1457b019baf08f2f98e20a
NetanelTz <zadok62@gmail.com>
2021-08-08 16:32:02 +0300

    first version initial_function.py

---
+++ initial_function.py
@@ -0,0 +1,15 @@
+import zipfile
+import os
+d = {}
+with zipfile.ZipFile("2021-archive.zip", "r") as f:
+    list_of_files = f.namelist()
+    print(list_of_files)
+    for elem in list_of_files:
+        ext = os.path.splitext(elem)[-1]
+        if ext == ".txt":
+            file1 = f.open(elem, 'r')
+            lines = file1.readlines()
+            for line in lines:
+                d[line.decode().replace("\n", "")] = elem
+
+print(d)
