b9d72e3eebb5cac22207e4b1f07403036aa361fe
NetanelTz <zadok62@gmail.com>
2021-08-09 16:19:01 +0300

    delete some

--- initial_function.py
+++ initial_function.py
@@ -1,8 +1,10 @@
 import zipfile
 import os
-from itertools import permutations
+
+
 d = {}
 d_final = {}
+
 
 def read_from_zip():
     with zipfile.ZipFile("wave.zip", "r") as f:
@@ -13,19 +15,14 @@ def read_from_zip():
                 file1 = f.open(elem, 'r')
                 lines = file1.readlines()
                 for line in lines:
-                    tmp = line.decode().replace("\n","").split()
+                    tmp = line.decode().replace("\n", "").split()
                     n = len(tmp)
-                    for i in range(n+1):
-                        for j in range(i+1, n+1):
-                           sli = " ".join(tmp[i:j])
-                           d[sli] = [" ".join(tmp), elem,i]
+                    for i in range(n + 1):
+                        for j in range(i + 1, n + 1):
+                            sli = " ".join(tmp[i:j])
+                            d[sli] = [" ".join(tmp), elem, i]
     for key in d:
         if len(str(key).split()) not in d_final.keys():
-            d_final[(len(str(key).split()))]=[]
-        d_final[(len(str(key).split()))].append({key:d[key]})
+            d_final[(len(str(key).split()))] = []
+        d_final[(len(str(key).split()))].append({key: d[key]})
 
-
-
-
-read_from_zip()
-print(d_final)
