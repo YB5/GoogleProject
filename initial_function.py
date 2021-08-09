53aa8744cd259c9b7bbc6b81b723c860c1b20e4a
NetanelTz <zadok62@gmail.com>
2021-08-09 12:26:46 +0300

    probably last version of init function

--- initial_function.py
+++ initial_function.py
@@ -1,10 +1,11 @@
 import zipfile
 import os
+from itertools import permutations
 d = {}
-
+d_final = {}
 
 def read_from_zip():
-    with zipfile.ZipFile("2021-archive.zip", "r") as f:
+    with zipfile.ZipFile("wave.zip", "r") as f:
         list_of_files = f.namelist()
         for elem in list_of_files:
             ext = os.path.splitext(elem)[-1]
@@ -12,5 +13,19 @@ def read_from_zip():
                 file1 = f.open(elem, 'r')
                 lines = file1.readlines()
                 for line in lines:
-                    d[line.decode().replace("\n", "")] = elem
+                    tmp = line.decode().replace("\n","").split()
+                    n = len(tmp)
+                    for i in range(n+1):
+                        for j in range(i+1, n+1):
+                           sli = " ".join(tmp[i:j])
+                           d[sli] = [" ".join(tmp), elem,i]
+    for key in d:
+        if len(str(key).split()) not in d_final.keys():
+            d_final[(len(str(key).split()))]=[]
+        d_final[(len(str(key).split()))].append({key:d[key]})
 
+
+
+
+read_from_zip()
+print(d_final)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import initial_function
 import AutoCompleteModule
 
+
 def main():
     print("Loading the files and preparing the system...")
     initial_function.read_from_zip()
@@ -8,6 +9,7 @@ def main():
     while True:
         text_to_search = input("Enter your text:\n")
         print(AutoCompleteModule.get_best_k_completions(text_to_search))
+
 
 if __name__ == '__main__':
     main()
