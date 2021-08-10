373181496b38a7000372e0e750ad141658017abf
YAAKOV BRIN <ybrin@g.jct.ac.il>
2021-08-10 14:21:16 +0300

    main

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ def main():
     initial_function.read_from_zip()
     print("The system is ready. ")
     while True:
-        text_to_search = input("Enter your text:\n")
+        text_to_search = input("Enter your text:\n").strip().lower()
         print(AutoCompleteModule.get_best_k_completions(text_to_search))
+        another_text_to_search = input(text_to_search)
+        while another_text_to_search != "#":
+            text_to_search = (text_to_search + another_text_to_search).strip().lower()
+            print(AutoCompleteModule.get_best_k_completions(text_to_search))
+            another_text_to_search = input(text_to_search)
 
 
 if __name__ == '__main__':
