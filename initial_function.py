d424eb2da9fda5e5a93b0c48273c8ae448aa14ab
YAAKOV BRIN <ybrin@g.jct.ac.il>
2021-08-10 12:41:19 +0300

    something

--- initial_function.py
+++ initial_function.py
@@ -6,28 +6,28 @@ d = {}
 d_final = {}
 
 
-class HandleLine(threading.Thread):
-    def __init__(self, f, elem):
-        super(HandleLine, self).__init__()
-        self.f = f
-        self.elem = elem
-        self.di = {}
-
-    def run(self):
-        lines = self.f
-        for line in lines:
-            tmp = line.decode().replace("\n", "").split()
-            n = len(tmp)
-            for i in range(n + 1):
-                for j in range(i + 1, n + 1):
-                    sli = " ".join(tmp[i:j])
-                    if sli not in self.di.keys():
-                        self.di[sli] = []
-                    self.di[sli].append([" ".join(tmp), self.elem, i])
+# class HandleLine(threading.Thread):
+#     def __init__(self, f, elem):
+#         super(HandleLine, self).__init__()
+#         self.f = f
+#         self.elem = elem
+#         self.di = {}
+#
+#     def run(self):
+#         lines = self.f
+#         for line in lines:
+#             tmp = line.decode().replace("\n", "").split()
+#             n = len(tmp)
+#             for i in range(n + 1):
+#                 for j in range(i + 1, n + 1):
+#                     sli = " ".join(tmp[i:j])
+#                     if sli not in self.di.keys():
+#                         self.di[sli] = []
+#                     self.di[sli].append([" ".join(tmp), self.elem, i])
 
 
 def read_from_zip():
-    with zipfile.ZipFile("2021-archive.zip", "r") as f:
+    with zipfile.ZipFile("wave.zip", "r") as f:
         list_of_files = f.namelist()
         for elem in list_of_files:
             ext = os.path.splitext(elem)[-1]
@@ -35,17 +35,26 @@ def read_from_zip():
                 print(elem)
                 file1 = f.open(elem, 'r')
                 lines = file1.readlines()
-                thr = []
-                for i in range(0, len(lines), len(lines) // 5):
-                    thred = HandleLine(lines[i:i + len(lines) // 5], elem)
-                    thr.append(thred)
-                for i in thr:
-                    i.start()
-
-    for i in thr:
-        i.join()
-    for i in thr:
-        d.update(i.di)
+                for line in lines:
+                    tmp = line.decode().replace("\n", "").split()
+                    n = len(tmp)
+                    for i in range(n + 1):
+                        for j in range(i + 1, n + 1):
+                            sli = " ".join(tmp[i:j])
+                            if sli not in d.keys():
+                                d[sli] = []
+                            d[sli].append([" ".join(tmp), elem, i])
+    #             thr = []
+    #             for i in range(0, len(lines), len(lines) // 5):
+    #                 thred = HandleLine(lines[i:i + len(lines) // 5], elem)
+    #                 thr.append(thred)
+    #             for i in thr:
+    #                 i.start()
+    #
+    # for i in thr:
+    #     i.join()
+    # for i in thr:
+    #     d.update(i.di)
     for key in d:
         if len(str(key).split()) not in d_final.keys():
             d_final[(len(str(key).split()))] = []
